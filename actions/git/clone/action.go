9bf27abb86bd5efa40b33b8c83d211e61e7d5620
Andy Esser <andy@flomation.co>
2026-03-25 22:58:41 +0000

    Clone into temp dir then move contents to execution directory
    go-git PlainClone requires an empty target. Clone into a temp
    directory, then walk the file tree and move all files/dirs into
    the current working directory. Temp dir cleaned up after move.

--- actions/git/clone/action.go
+++ actions/git/clone/action.go
@@ -1,6 +1,10 @@
 package git_clone
 
 import (
+	"io/fs"
+	"os"
+	"path/filepath"
+
 	core "flomation.app/automate/executor"
 	"github.com/go-git/go-git/v6"
 	"github.com/go-git/go-git/v6/plumbing/transport/ssh"
@@ -56,7 +60,14 @@ func Execute(flow *core.Flow, node *core.Node, inputs []*core.Connection) (map[s
 		cloneOpts.Auth = pk
 	}
 
-	repo, err := git.PlainClone(".", cloneOpts)
+	// Clone into a temp directory then move contents into the execution directory
+	tmpDir, err := os.MkdirTemp("", "flomation-clone-*")
+	if err != nil {
+		return nil, err
+	}
+	defer os.RemoveAll(tmpDir)
+
+	repo, err := git.PlainClone(tmpDir, cloneOpts)
 	if err != nil {
 		return nil, err
 	}
@@ -65,6 +76,35 @@ func Execute(flow *core.Flow, node *core.Node, inputs []*core.Connection) (map[s
 	head, err := repo.Head()
 	if err == nil && head != nil {
 		branch = head.Name().Short()
+	}
+
+	// Move all cloned files into the current working directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, err
+	}
+
+	err = filepath.WalkDir(tmpDir, func(path string, d fs.DirEntry, walkErr error) error {
+		if walkErr != nil {
+			return walkErr
+		}
+
+		rel, err := filepath.Rel(tmpDir, path)
+		if err != nil {
+			return err
+		}
+		if rel == "." {
+			return nil
+		}
+
+		dest := filepath.Join(cwd, rel)
+		if d.IsDir() {
+			return os.MkdirAll(dest, 0755)
+		}
+		return os.Rename(path, dest)
+	})
+	if err != nil {
+		return nil, err
 	}
 
 	return map[string]interface{}{
