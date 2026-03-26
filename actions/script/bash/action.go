445d895a9fa5652b29a3ea3cfe61a0bdf6faf2fc
Andy Esser <andy@flomation.co>
2026-03-26 00:21:57 +0000

    Fail flow on non-zero bash script exit code

--- actions/script/bash/action.go
+++ actions/script/bash/action.go
@@ -170,11 +170,21 @@ func Execute(flow *core.Flow, node *core.Node, inputs []*core.Connection) (map[s
 		}
 	}
 
+	if exitCode != 0 {
+		return map[string]interface{}{
+			"stdout":            strings.TrimSpace(stdoutStr),
+			"stderr":            strings.TrimSpace(stderrStr),
+			"exit_code":         exitCode,
+			"success":           false,
+			"working_directory": workDir,
+		}, fmt.Errorf("script exited with code %d: %s", exitCode, strings.TrimSpace(stderrStr))
+	}
+
 	return map[string]interface{}{
 		"stdout":            strings.TrimSpace(stdoutStr),
 		"stderr":            strings.TrimSpace(stderrStr),
 		"exit_code":         exitCode,
-		"success":           exitCode == 0,
+		"success":           true,
 		"working_directory": workDir,
 	}, nil
 }
